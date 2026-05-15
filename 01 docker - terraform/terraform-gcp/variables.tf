bcc1f18a39efdbbbbe2036385a1fbec8afa901cb
Mark Anthony Baltazar <baltazar.markanthony@gmail.com>
2026-05-15 04:19:58 +0000

    added terraorm

---
+++ 01 docker - terraform/terraform-gcp/variables.tf
@@ -0,0 +1,41 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my-creds.json"
+  #directory of credential file
+}
+
+
+variable "project" {
+  description = "Project"
+  #default project name, ex: my-project-123456
+  default     = "de-course-496106"
+}
+
+variable "region" {
+  description = "Region"
+  #your region, ex: us-central1, us-east1, etc
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  #your location, ex: US, EU, etc
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #name of the dataset 
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #name of bucket must be globally unique.
+  default     = "terraform-demo-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
